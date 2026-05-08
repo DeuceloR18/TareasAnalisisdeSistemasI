121b14b1dbe9d315bf8af846ba982b79afd73cc2
DeuceloR18 <daniloucelo087@gmail.com>
2026-05-08 17:05:36 -0600

    Patron Singleton

---
+++ Singlenton.cs
@@ -0,0 +1,17 @@
+class Singleton
+{
+    private static Singleton instancia;
+
+    private Singleton() { }
+
+    public static Singleton Instancia
+    {
+        get
+        {
+            if (instancia == null)
+                instancia = new Singleton();
+
+            return instancia;
+        }
+    }
+}
