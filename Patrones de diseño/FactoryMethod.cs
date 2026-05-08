f707e96fb9c94d378264618bc2a144348f516cca
DeuceloR18 <daniloucelo087@gmail.com>
2026-05-08 16:36:54 -0600

    Patron de diseño Factory Method

---
+++ Patrones de diseño/FactoryMethod.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patrones_de_diseño
+{
+    using System;
+
+    interface IVehiculo
+    {
+        void Mostrar();
+    }
+
+    class Carro : IVehiculo
+    {
+        public void Mostrar()
+        {
+            Console.WriteLine("Se creó un carro");
+        }
+    }
+
+    class Moto : IVehiculo
+    {
+        public void Mostrar()
+        {
+            Console.WriteLine("Se creó una moto");
+        }
+    }
+
+    class FabricaVehiculo
+    {
+        public static IVehiculo Crear(string tipo)
+        {
+            if (tipo == "carro")
+                return new Carro();
+
+            return new Moto();
+        }
+    }
+}
